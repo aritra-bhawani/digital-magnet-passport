f48cb1751e696393468c79ff49411290d1401eb1
aritra-bhawani <eman.arghya@gmail.com>
2026-06-30 06:15:54 +0100

    Initial commit: Initialization of fast-api

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from app.api.v1.router import router as v1_router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.debug,
+)
+
+app.include_router(v1_router, prefix=settings.api_v1_prefix)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Digital Magnet Passport API"}

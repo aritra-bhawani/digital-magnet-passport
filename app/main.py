0ae601105bdebfe1ce1de08a5e9b9163f4f927db
aritra-bhawani <eman.arghya@gmail.com>
2026-07-02 09:57:23 +0100

    Included docker and directory setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.v1.router import router as v1_router
-from app.core.config import settings
+from app.config import settings
 
 app = FastAPI(
     title=settings.app_name,
